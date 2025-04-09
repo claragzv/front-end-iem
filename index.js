84d17df38d1224fa94ff92c3c100da2133f13972
claragzv <claragzv@gmail.com>
2025-04-09 20:59:50 +0200

    Added class one files

---
+++ index.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("counter-button");
+let count = 0;
+const countSpan = document.getElementById("count");
+
+countSpan.innerText = count;
+
+console.log(button);
+
+button.addEventListener("click", () => {});
